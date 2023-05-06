92699f05499c746f48d6d5f88f8169fc543148f6
Juan Cruz Atamañuk <105613450+juanatama1132@users.noreply.github.com>
2023-05-06 13:04:44 -0300

    Delete 16-03-23/src directory

--- 16-03-23/src/utils/bCrypt.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt";
-const createHash = (Password) =>
-  bcrypt.hashSync(password, bcrypt.genSaltSync(15));
-const isPwdValid = (user, password) =>
-  bcrypt.compareSync(password, user.password);
-module.exports = { createHash, isPwdValid };
